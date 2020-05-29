4d642977935ffbd6fa49f36ca2ddb719eb327a93
lanurag150 <60335727+lanurag150@users.noreply.github.com>
2020-05-29 19:49:41 +0530

    Update index.php

--- templates/index.php
+++ templates/index.php
@@ -78,12 +78,12 @@
       </div>
     </div>
 
-   <section class="ftco-section contact-section ftco-degree-bg" style="height: 800px">
+   <section class="ftco-section contact-section ftco-degree-bg" style="height: 680px">
    
       <div class="container col-lg-6" >
           
           
-          <div class="w3-card-4" style="width: 700px; height:500px; padding:20px">
+          <div class="w3-card-4" style="width: 700px; height:550px; padding:20px">
               <div class="w3-container w3-black">
                 <h2 style="color:white;">Pneumonia Analysis</h2>
               </div>
